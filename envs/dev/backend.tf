42c0f491575fcc38156316c5eeed16bd64bca07b
bhojeshwarsahu <bhojeshwarsahu123@gmail.com>
2025-11-19 20:59:28 +0530

    Remove optional DynamoDB table configuration comment from backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -4,7 +4,5 @@ terraform {
     key          = "dev/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
-    # Optional: if you want state locking you should configure a DynamoDB table:
-    # dynamodb_table = "terraform-state-locks"
   }
 }
